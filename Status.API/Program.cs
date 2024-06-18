20bfe0083d99b19d11405da24c1cfea632b0f073
Sebastian Meyer <sebastian.meyer@meyer-itconsulting.de>
2024-06-18 20:06:37 +0200

    Component updates

--- Status.API/Program.cs
+++ Status.API/Program.cs
@@ -11,7 +11,9 @@ app.UseHttpsRedirection();
 
 app.MapGet("/status/{orderId}", async (string orderId) =>
 {
+    Console.WriteLine($"Status requested for {orderId}");
     string status = await daprClient.GetStateAsync<string>(DAPR_STATE_STORE, orderId);
+    Console.WriteLine($"Status for Order {orderId} is {status}");
 
     return Results.Ok(new StatusMessage(orderId, status));
 });
